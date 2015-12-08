28126cd596467e59c790b7c4123d4ce354228c43
misc <MiSc@ORD01777.ordina.belgium>
2015-12-08 22:38:15 +0100

    Use TransientItemProviderAdapter

--- projects.cpt.sortingplans/projects.cpt.sortingplans.edit/src/com/misc/projects/SortingPlans/provider/TransientScenarioItemProvider.java
+++ projects.cpt.sortingplans/projects.cpt.sortingplans.edit/src/com/misc/projects/SortingPlans/provider/TransientScenarioItemProvider.java
@@ -1,28 +1,25 @@
 package com.misc.projects.SortingPlans.provider;
 
-import java.util.Collection;
 
-import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.ResourceLocator;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
-import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
+import com.misc.common.moplaf.emf.edit.ui.provider.TransientItemProviderAdapter;
 import com.misc.projects.SortingPlans.Scenario;
 
-public class TransientScenarioItemProvider extends ItemProviderAdapter
+
+abstract public class TransientScenarioItemProvider extends TransientItemProviderAdapter
 		implements IEditingDomainItemProvider, 
 		           IStructuredItemContentProvider,
 		           ITreeItemContentProvider, 
 		           IItemLabelProvider, 
 		           IItemPropertySource {
+
 
 	// constructor
 	public TransientScenarioItemProvider(AdapterFactory adapterFactory, Scenario scenario) {
@@ -30,58 +27,13 @@ public class TransientScenarioItemProvider extends ItemProviderAdapter
 		scenario.eAdapters().add(this);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getChildren(java.lang.Object)
-	 */
-	@Override
-	public Collection<?> getChildren(Object object) {
-		return super.getChildren(this.target);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getParent(java.lang.Object)
-	 */
-	@Override
-	public Object getParent(Object object) {
-		return this.target;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getFeatureValue(org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EStructuralFeature)
-	 */
-	@Override
-	protected Object getFeatureValue(EObject object, EStructuralFeature feature) {
-		return super.getFeatureValue((EObject) this.target, feature);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getNewChildDescriptors(java.lang.Object, org.eclipse.emf.edit.domain.EditingDomain, java.lang.Object)
-	 */
-	@Override
-	public Collection<?> getNewChildDescriptors(Object object,
-			EditingDomain editingDomain, Object sibling) {
-		return super.getNewChildDescriptors(this.target, editingDomain, sibling);
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#createRemoveCommand(org.eclipse.emf.edit.domain.EditingDomain, org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EStructuralFeature, java.util.Collection)
-	 */
-	@Override
-	protected Command createRemoveCommand(EditingDomain domain, EObject owner,
-			EStructuralFeature feature, Collection<?> collection) {
-		return super.createRemoveCommand(domain, (EObject) this.target, feature, collection);
-	}
-
 	/**
 	 * Return the resource locator for this item provider's resources.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
 	 */
 	@Override
 	public ResourceLocator getResourceLocator() {
 		return SortingplansEditPlugin.INSTANCE;
 	}
-	
-
 }
--- projects.cpt.sortingplans/projects.cpt.sortingplans.edit/src/com/misc/projects/SortingPlans/provider/TransientSortingPlanItemProvider.java
+++ projects.cpt.sortingplans/projects.cpt.sortingplans.edit/src/com/misc/projects/SortingPlans/provider/TransientSortingPlanItemProvider.java
@@ -1,20 +1,17 @@
 package com.misc.projects.SortingPlans.provider;
 
-import java.util.Collection;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.ResourceLocator;
-import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
-import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
+import com.misc.common.moplaf.emf.edit.ui.provider.TransientItemProviderAdapter;
 import com.misc.projects.SortingPlans.SortingPlan;
 
-public class TransientSortingPlanItemProvider extends ItemProviderAdapter
+public class TransientSortingPlanItemProvider extends TransientItemProviderAdapter
 		implements IEditingDomainItemProvider, 
 		           IStructuredItemContentProvider,
 		           ITreeItemContentProvider, 
@@ -27,32 +24,6 @@ public class TransientSortingPlanItemProvider extends ItemProviderAdapter
 		sortingPlan.eAdapters().add(this);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getChildren(java.lang.Object)
-	 */
-	@Override
-	public Collection<?> getChildren(Object object) {
-		return super.getChildren(this.target);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getParent(java.lang.Object)
-	 */
-	@Override
-	public Object getParent(Object object) {
-		return this.target;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderAdapter#getNewChildDescriptors(java.lang.Object, org.eclipse.emf.edit.domain.EditingDomain, java.lang.Object)
-	 */
-	@Override
-	public Collection<?> getNewChildDescriptors(Object object,
-			EditingDomain editingDomain, Object sibling) {
-		// TODO Auto-generated method stub
-		return super.getNewChildDescriptors(this.target, editingDomain, sibling);
-	}
-	
 	/**
 	 * Return the resource locator for this item provider's resources.
 	 * <!-- begin-user-doc -->
